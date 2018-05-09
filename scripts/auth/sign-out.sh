6309cd4c403115a00d0eeceb80c5055dd6f0787c
PatODay <poday22@gmail.com>
2018-05-09 10:47:33 -0400

    Confirmed auth CRUD works & create Page models
    Curl scripts command at the top of each file
    Dependencies installed
    Packages installed:
      * nodemon

--- scripts/auth/sign-out.sh
+++ scripts/auth/sign-out.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# sh scripts/auth/sign-out.sh
 
 API="http://localhost:4741"
 URL_PATH="/sign-out"
--- scripts/auth/sign-up.sh
+++ scripts/auth/sign-up.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# sh scripts/auth/sign-up.sh
 
 API="http://localhost:4741"
 URL_PATH="/sign-up"
